167e21bffd0fb567685632ccbabff3a53fc35f33
binisha <sherchanbinisha5@gmail.com>
2022-12-28 15:00:52 +0545

    Add event

--- index.php
+++ index.php
@@ -420,7 +420,7 @@
                    <div class="card">
                      <div class="content">
                       <div class="front">
-                          <div class="img d-flex align-items-end" style="background-image: url(img/features/queen-tower.jpg);">
+                          <div class="img d-flex align-items-end" style="background-image: url(img/features/queen-tower3.png);">
                              <div class="text w-100">
 									        	  	<h3><a href="#">5 star queen tower</a></h3>
 									       	   </div>
@@ -729,6 +729,160 @@
    <!-- client single item end -->
     </div>
   </div>
+</section>
+
+<!-- impact  -->
+<section id="impact_stories">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <h6>latest events</h6>
+           <h2 class="section-title">Latest Events</h2>
+
+            <div class="media_content mb-5">
+                <div class="row main_media">
+                    <div class="col-lg-12">
+                        <div class="media_img" data-aos="fade-right" data-aos-duration="2000" style="height: 300px;">
+                            <video autoplay muted loop playsinline preload="metadata" controls="controls" height="100%">
+                                <source src="img/videos/egm.mp4"  type="video/mp4" >
+                            </video>
+<!--                            <h2 class="breadcrumb-title"> bandipur</h2>-->
+                        </div>
+                        <div class="media_content_right ps-3 pe-3" data-aos="fade-left" data-aos-duration="1200">
+                            <h2>EGM (Extraordinary General Meeting)</h2>
+                            <small>7th Shrawan, 2079 </small>
+                            <p>RKD Holdings Limited held an EGM (Extraordinary General Meeting) at Red House, Bandipur
+                                    4, Tundhikhel on Shrawan 7, Monday. The programs main motive was about RKD Business
+                                    Strategic Assembly. It focused on theme such as CM Boss, Salami, Ilami and upcoming projects
+                                    of Bandipur Cable Car. The speech and presentation was given by the directors, Mr. Ramchandra
+                                    Sharma, Mr. Shiva Prasad Sharma, Mr. Chintamani Sharma, Mr. Kiran Pokhrel, and many more.
+                                    The assembly went smooth and was very effective among the individuals. The program also
+                                    conducted interaction activities between the shareholders which helped to bring out the result in
+                                    a systematic and effective manner.</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+           <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit sapiente rerum qui numquam. Cumque quo in esse, adipisci ducimus eaque ea vitae, praesentium facilis molestiae error dolorem modi iusto.</p> -->
+           <!-- impact stories card column -->
+            <div class="row">
+        <div class="col-lg-3">
+           <a href="impact-details1.php">
+           <div class="card">
+           <div class="backgroundEffect"></div>
+            <div class="pic"> <img class="" src="img/bids.jpg" alt="">
+                <div class="date"> <span class="day">12</span> <span class="month">Poush</span> <span class="year">2079</span> </div>
+            </div>
+            <div class="content">
+               <h4 class="impact_heading">International Invitation for E-bids</h4>
+                <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
+                   <a href="impact-details1.php"> Read More</a>
+                    <a href="impact-details1.php" class="impact_one_arrow"><i class="icon-long-arrow-right"></i></a>
+
+                </div>
+            </div>
+           </div>    
+             <!--card end  -->        
+           </a>
+        </div>
+        <div class="col-lg-3">
+           <a href="impact-details2.php">
+              <div class="card">
+                   <div class="backgroundEffect"></div>
+                   <div class="pic"> <img class="" src="img/events/event-2.jpg" alt="">
+                        <div class="date"> <span class="day">17</span> <span class="month">Sep</span> <span class="year">2021</span> </div>
+                   </div>
+                   <div class="content">
+                        <h4 class="impact_heading">Call for 7th AGM(Annual General Meeting) of Bandipur Cable Car Company</h4>
+                        <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
+                             <a href="impact-details2.php"> Read More</a>                                 
+                             <a href="impact-details2.php" class="impact_one_arrow"><i class="icon-long-arrow-right"></i></a>                            
+                        </div>
+                    </div>
+             </div>  
+               <!--card end  -->
+          </a>          
+        </div>
+        <div class="col-lg-3">
+            <a href="impact-details3.php">
+               <div class="card">
+                   <div class="backgroundEffect"></div>
+                   <div class="pic"> <img class="" src="img/events/event-3.jpg" alt="">
+                       <div class="date"> <span class="day">09</span> <span class="month">Mangsir</span> <span class="year">2077</span> </div>
+                   </div>
+                  <div class="content">
+                    <h4 class="impact_heading">Bandipur Cable Car financial closure from Nepal SBI & NMB Bank</h4>
+                     <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
+                        <a href="impact-details3.php"> Read More</a>                    
+                        <a href="impact-details3.php" class="impact_one_arrow"><i class="icon-long-arrow-right"></i></a>                    
+                     </div>
+               </div>
+           </div>  
+             <!--card end  -->
+          </a>          
+        </div>
+        <div class="col-lg-3">
+            <a href="impact-details4.php">
+               <div class="card">
+                    <div class="backgroundEffect"></div>
+                    <div class="pic"> <img class="" src="img/events/event-4.jpg" alt="">
+                         <div class="date"> <span class="day">25</span> <span class="month">Magh</span> <span class="year">2076</span> </div>
+                    </div>
+                    <div class="content">
+                        <h4 class="impact_heading">5th Annual General Meeting Decision of Bandipur Cable Car</h4>
+                        <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
+                              <a href="impact-details4.php"> Read More</a>
+                              <a href="impact-details4.php" class="impact_one_arrow"><i class="icon-long-arrow-right"></i></a>                          
+                        </div>
+                   </div>
+               </div>  
+                <!--card end  -->
+            </a>         
+        </div>     
+    </div>
+</section>
+<section id="events">
+   <div class="container"> 
+      <div class="row">
+      <div class="event-area">
+  <div class="row">
+    <div class="col-md-12"> 
+      <h2 class="section-title">Upcoming Events</h2>
+    </div>
+    <div class="col-md-12">
+      <div class="event-box d-flex">
+        <div class="event-date text-center">
+          <p>25 March</p>
+        </div>
+        <div class="event-details">
+          <h5><a href="/event-details">2020 admidion fair going on this week</a></h5>
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item"><i class="icon-clock-o"></i>5:30pm - 7.00pm</li>
+              <li class="list-inline-item"><i class="icon-map-marker"></i>Newyork,NY.</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet consectetur. Como Ssee hou</p>
+          </div>
+        </div>
+        <div class="event-box d-flex">
+          <div class="event-date text-center">
+            <p>25 March</p>
+          </div>
+          <div class="event-details">
+            <h5><a href="/event-details">2020 admidion fair going on this week</a></h5>
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item"><i class="icon-clock-o"></i>5:30pm - 7.00pm</li>
+              <li class="list-inline-item"><i class="icon-map-marker"></i>Newyork,NY.</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet consectetur. Como Ssee hou</p>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
+      </div>
+   </div>
 </section>
 <!-- <section id="coops_principle">
     <div class="container">
