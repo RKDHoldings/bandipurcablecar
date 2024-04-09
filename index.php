c6b695060df517aa524797637947b33d2eb5ea86
binisha <sherchanbinisha5@gmail.com>
2024-04-09 16:41:09 +0545

    Add popup

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ include('includes/html_header.php');
 include('includes/navbar.php');
 ?>
 
-  <!-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-toggle="modal">
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-toggle="modal">
       <div class="modal-dialog">
             <div class="modal-content" >
                   <div class="modal-header text-center">
-                        <h4>Shivaratri Special</h4>
+                        <!-- <h4>Shivaratri Special</h4> -->
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Close"></button>
                   </div>
                   <div class="modal-body">        
-                       <img src="img/shivratri-post.png" width="100%" alt="">         
+                       <img src="img/opening.jpg" width="100%" alt="">         
                   </div>    
              
             </div>
        </div>
-  </div> -->
+  </div>
 
 <section id="banner">
     <div class="content">
