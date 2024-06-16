8c48e3bab92e9f7e8328e2e85030661c07c1f01c
binisha <sherchanbinisha5@gmail.com>
2024-06-16 11:45:24 +0545

    Add new popup

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ include('includes/html_header.php');
 include('includes/navbar.php');
 ?>
 
-  <!-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-toggle="modal">
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-toggle="modal">
       <div class="modal-dialog">
             <div class="modal-content" >
                   <div class="modal-header text-center">
-                        <h4>Heartfelt Invitation</h4>
+                        <!-- <h4>Heartfelt Invitation</h4> -->
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Close"></button>
                   </div>
                   <div class="modal-body">        
-                       <img src="img/invitation.jpg" width="100%" alt="">         
+                       <img src="img/launch.jpeg" width="100%" alt="">         
                   </div>    
              
             </div>
        </div>
-  </div> -->
+  </div>
 
 <section id="banner">
     <div class="content">
