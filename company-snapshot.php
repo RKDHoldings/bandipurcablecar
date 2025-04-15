b158c9f49282965968fb6bdf677c75b44498bb55
sailesh acharya <sailesh.acharya@bizbazar.com>
2025-04-15 17:28:28 +0545

    Add new page

---
+++ company-snapshot.php
@@ -0,0 +1,147 @@
+<?php
+ include('includes/html_header.php');
+ ?>
+ <?php
+ include('includes/navbar.php');
+ ?>
+  <!-- banner -->
+ <section id="breadcrumb">
+         <div class="bg-overlay pt-opacity4" style="background-image: url('img/service4-bg.webp');"></div>
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <h2 class="breadcrumb-title">award and recognition</h2>
+                  <nav aria-label="breadcrumb">
+                     <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php"><i data-feather="home"></i>Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">award and recognition</li>
+                     </ol>
+                  </nav>
+               </div>
+            </div>
+         </div>
+</section>
+<section id="award_recognition">
+   <div class="container">
+     <h2 class="section-title">COMPANY SNAPSHOT</h2>
+     <p>As of Ashar 31, 2081</p>
+     <div class="row care_rating">
+         <div class="col-lg-12">
+         <table class="company-table">
+            <tr>
+                <th class="table-title">Company Category</th>
+                <td class="table-details">Public Limited</td>
+            </tr>
+            <tr>
+                <th class="table-title">Registered Office</th>
+                <td class="table-details">Kalimati-13, Kathmandu</td>
+            </tr>
+            <tr>
+                <th class="table-title">Field Office</th>
+                <td class="table-details">Bandipur-4, Tanahun</td>
+            </tr>
+            <tr>
+                <th class="table-title">Company Registration Date</th>
+                <td class="table-details">Registered as Private Limited Company: Push 22, 2069<br>
+                    Converted to Public Limited Company: Bhadra 17, 2078</td>
+            </tr>
+            <tr>
+                <th class="table-title">Company Registration No</th>
+                <td class="table-details">270364/078/079</td>
+            </tr>
+            <tr>
+                <th class="table-title">Permanent Account No. (PAN)</th>
+                <td class="table-details">600918925</td>
+            </tr>
+            <tr>
+                <th class="table-title">Objectives</th>
+                <td class="table-details">
+                i. Cable car operation with well-equipped Tourism Infrastructure<br>
+                ii. Well-equipped Hotel operation with quality assurance<br>
+                iii. Well-equipped sightseeing center<br>
+                iv. Convenience restaurant operation<br>
+                v. Investment in recreation, adventure, tourism infrastructure
+                </td>
+            </tr>
+            <tr>
+                <th class="table-title">Area of operation</th>
+                <td class="table-details">Tourism, Hospitality, Adventure and Recreational activities</td>
+            </tr>
+            <tr>
+                <th class="table-title">Cable car ropeway distance</th>
+                <td class="table-details">1616 meters</td>
+            </tr>
+            <tr>
+                <th class="table-title">Hotel Accommodation</th>
+                <td class="table-details">80 Keys</td>
+            </tr>
+            <tr>
+                <th class="table-title">No. of Shareholders</th>
+                <td class="table-details">5670</td>
+            </tr>
+            <tr>
+                <th class="table-title">No. of employees</th>
+                <td class="table-details">74</td>
+            </tr>
+            <tr>
+                <th class="table-title">
+                    Other information <br>
+                    Website: <br>
+                    Email:
+                </th>
+                <td class="table-details">
+                    <br>
+                <a href="http://www.bandipurhill.com" target="_blank">www.bandipurhill.com</a><br>
+                <a href="mailto:info@bandipurhill.com">info@bandipurhill.com</a>
+                </td>
+            </tr>
+            </table>
+            <!-- <img src="img/company-snapshot.png" alt="company-details"> -->
+         </div>
+     </div>  
+</div>
+</section>
+
+<section id="award_details">
+    <div class="container">
+        <div class="row">
+           <h6>Capital Structure</h6>
+           
+           <p>Bandipur Cable Car & Tourism Limited not only focus on business, company also works for betterment of society. Company has taken following steps to fulfill It's corporate social responsibility:  </p>       
+           <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="award-details.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="img/news/award/startup.jpg" alt="" />
+                                </div>
+                                <div class="text-container">
+                                    <h5>बन्दीपुर केवलकार पर्यटन एवं उद्यमशील पत्रकारिता पुरस्कार तथा हार्दिक सम्मान</h5>
+                                    <!-- <p>कम्पनीले बन्दीपुर केवलकार पर्यटन एवं उद्यमशील पत्रकारिता पुरस्कार–२०७६ स्थापना गरेको थियो ।</p> -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <a href="award-details2.php">
+                        <div class="card-flyer">
+                            <div class="text-box">
+                                <div class="image-box">
+                                    <img src="img/news/award/award.jpg" alt="" />
+                                </div>
+                                <div class="text-container">                                    
+                                     <h5> लाइफ टाइम फेयर टिकट बिक्री</h5>
+                                    <!-- <p>कम्पनीले स्थानीय बासिन्दाका लागि केवलकारमा सवार गरेवापत् जीवन भरीका लागिसुरुवातमा १० वर्षको अवधि रहने गरी लाइफ टाईम फेयर घोषणा गरको छ ।</p> -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>              
+        </div>
+    </div>  
+</section>
+
+<?php
+ include('includes/footer.php');
+ ?>
